e9b6b4899bcab047783e543694b20e90cb926d9a
TristanDspt <tristan.despont@hotmail.fr>
2025-07-29 13:33:32 +0200

    minor changes

--- fichiers.py
+++ fichiers.py
@@ -5,12 +5,12 @@ def ouvrir_fichier_local(filename, mode='r', encoding=None):
     Ouvre un fichier situé dans le même dossier que ce script.
     
     Args:
-        filename (str): nom du fichier à ouvrir.
-        mode (str): mode d'ouverture (par défaut 'r').
-        encoding (str, optionnel): encodage à utiliser (ex: 'utf-8').
+        filename (str): Nom du fichier à ouvrir.
+        mode (str): Mode d'ouverture (par défaut 'r').
+        encoding (str, optionnel): Encodage à utiliser (ex: 'utf-8').
     
     Returns:
-        file object: fichier ouvert prêt à être lu ou écrit.
+        file object: Fichier ouvert prêt à être lu ou écrit.
     """
     chemin_script = os.path.dirname(os.path.abspath(__file__))
     chemin_fichier = os.path.join(chemin_script, filename)
@@ -24,10 +24,10 @@ def get_nom_fichier(filename):
     Récupère le nom d'un fichier sans son extension.
     
     Args:
-        filename (str): nom du fichier à ouvrir.
+        filename (str): Nom du fichier à ouvrir.
         
     Returns:
-        nom_entree: fichier sans l'extension.
+        nom_entree: Fichier sans l'extension.
     """
     nom_entree = os.path.splitext(filename)[0].lower()
     return nom_entree
@@ -37,10 +37,10 @@ def get_extension_fichier(filename):
     Récupère l'extension d'un fichier.
     
     Args:
-        filename (str): nom du fichier à ouvrir.
+        filename (str): Nom du fichier à ouvrir.
         
     Returns:
-        filetype (str): extension.
+        filetype (str): Extension.
     """
     filetype = os.path.splitext(filename)[1].lower()
     return filetype
