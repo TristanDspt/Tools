bd7797d6ac88ca2d8d14d01eae113293523faa0b
TristanDspt <tristan.despont@hotmail.fr>
2026-03-20 13:12:42 +0100

    refacto mineure

--- data/distribution.py
+++ data/distribution.py
@@ -1,7 +1,7 @@
 import pandas as pd
 import numpy as np
 import matplotlib.pyplot as plt
-from data.utils import verification_donnees
+from data.utils_stats import verification_donnees
 
 def courbe_lorenz(df, var1, titre_x, titre_y):
     """
