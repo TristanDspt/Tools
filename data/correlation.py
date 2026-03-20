55f1aa20e7d191b80c9db6c48eeabbe762777da7
TristanDspt <tristan.despont@hotmail.fr>
2026-03-20 19:51:34 +0100

    feat : corr quali vs quali

--- data/correlation.py
+++ data/correlation.py
@@ -26,10 +26,14 @@ Notes :
 # --------------------------------------------------------------------------------------------------------------------
 
 # Import des bibliothèques
+import pandas as pd
+import numpy as np
+import matplotlib.pyplot as plt
+import seaborn as sns
 import plotly.express as px
 
 # Bibliothèques statistiques
-from scipy.stats import pearsonr, spearmanr
+from scipy.stats import pearsonr, spearmanr, chi2_contingency
 
 # Fichiers de dépendances
 from data.utils_stats import verification_donnees, verification_normalite
@@ -56,7 +60,7 @@ def correlation_quanti_quanti(df, var1, var2, seuil=0.05, plot=True, report=True
         >>> correlation_quanti_quanti(df, 'age', 'montant')
         >>> correlation_quanti_quanti(df, 'age', 'montant', seuil=0.01, plot=False)
     """
-    
+
     # Initialisation de la fonction --------------------------------------
     print("=" * 70)
     print(f"📊 ANALYSE DE CORRÉLATION")
@@ -68,7 +72,8 @@ def correlation_quanti_quanti(df, var1, var2, seuil=0.05, plot=True, report=True
     verif = verification_donnees(df, var1, var2)
     print(f"\n🔍 Analyse de la corrélation entre '{var1}' (type : {verif['dtype_var1']}) et '{var2}' (type : {verif['dtype_var2']})")
     
-    if verif['type_df'] == 'DataFrame' and verif['dtype_var1'] in ['int64', 'float64'] and verif['dtype_var2'] in ['int64', 'float64'] and verif['shape'][0] > 2:
+    liste_type = ['int64', 'float64']
+    if verif['type_df'] == 'DataFrame' and verif['dtype_var1'] in liste_type and verif['dtype_var2'] in liste_type and verif['shape'][0] > 2:
         print(f"✅ Vérification des données : OK, les variables sont quantitatives et le dataset est suffisant pour l'analyse.")
     else:
         print(f"❌ Vérification des données : Les variables doivent être quantitatives (int64 ou float64) et le dataset doit contenir plus de 2 lignes pour effectuer une analyse de corrélation.")
@@ -139,6 +144,92 @@ def correlation_quanti_quanti(df, var1, var2, seuil=0.05, plot=True, report=True
         print(f"📊 Coefficient de corrélation : {corr:.4f}, corrélation {'faible' if abs(corr) <= 0.3 else 'modérée' if 0.3 < abs(corr) <= 0.7 else 'forte'}")
         print(f"📊 P-value : {p_value:.4f}")
         print(f"📊 R² : {r_carre:.2f} -> '{var1}' explique {r_carre * 100:.0f}% de la variance de '{var2}'")
+
+        if p_value < seuil:
+            print(f"\n✅ CONCLUSION: Il existe une corrélation statistiquement significative entre '{var1}' et '{var2}' (p < {seuil}).")
+        else:
+            print(f"\n⚠️ CONCLUSION : Il n'existe pas de corrélation statistiquement significative entre '{var1}' et '{var2}' (p >= {seuil}).")
+
+
+
+def correlation_quali_quali(df, var1, var2, seuil=0.05, plot=True, report=True):
+    # Initialisation de la fonction --------------------------------------
+    print("=" * 70)
+    print(f"📊 ANALYSE DE CORRÉLATION")
+    print("=" * 70)
+
+
+    # Step 1 : vérification préliminaire des données --------------------------------------
+    print("\n" + "-" * 70)
+    verif = verification_donnees(df, var1, var2)
+    print(f"\n🔍 Analyse de la corrélation entre '{var1}' (type : {verif['dtype_var1']}) et '{var2}' (type : {verif['dtype_var2']})")
+    
+    liste_type = ['object', 'category']
+    if verif['type_df'] == 'DataFrame' and verif['dtype_var1'] in liste_type and verif['dtype_var2'] in liste_type and verif['shape'][0] > 2:
+        print(f"✅ Vérification des données : OK, les variables sont qualitatives et le dataset est suffisant pour l'analyse.")
+    else:
+        print(f"❌ Vérification des données : Les variables doivent être qualitatives (object ou category) et le dataset doit contenir plus de 2 lignes pour effectuer une analyse de corrélation.")
+        return    
+
+
+    # Step 2 : Vérification de la normalité des variables --------------------------------------
+    # None
+
+
+    # Step 3 : Calcul de la corrélation --------------------------------------
+    print("\n" + "-" * 70)
+
+    tab = pd.crosstab(df[var1], df[var2])
+    chi2, p_value, dof, exp = chi2_contingency(tab)
+    v_cramer = np.sqrt(chi2 / (df.shape[0] * (min(df[var1].nunique(), df[var2].nunique()) - 1)))
+    
+    if p_value < seuil:
+        print(f"\n✅ La corrélation est statistiquement significative (p < {seuil}).")
+    else:
+        print(f"\n⚠️ La corrélation n'est pas statistiquement significative (p >= {seuil}).")
+
+
+    # Step 4 : Visualisation de la corrélation --------------------------------------
+    if plot:
+        if max(df[var1].nunique(), df[var2].nunique()) <= 5:
+            # Barplot
+            tab_graph = tab.reset_index().melt(id_vars=var1).copy()
+
+            sns.barplot(
+                tab_graph,
+                x=var2,
+                y='value',
+                hue=var1,
+                palette='viridis'
+            )
+        else:
+            # Heatmap
+            sns.heatmap(
+                tab,
+                annot=True, 
+                fmt='d', 
+                cmap='Blues',
+                xticklabels=tab.columns,
+                yticklabels=tab.index, 
+                linewidths=2
+                )
+
+        plt.title("Corrélation Chi2", fontname='Arial' , fontweight='bold', fontsize=16, x=0.51, ha='center')
+        plt.tick_params(axis='x', labelsize=11.5)
+        plt.tick_params(axis='y', labelsize=11.5)
+
+        plt.show()
+
+
+    # Step 5 : Rapport & Conclusion --------------------------------------
+    if report:
+        print(f"=" * 70)
+        print(f"📊 RAPPORT DE CORRÉLATION ENTRE '{var1}' ET '{var2}'")
+        print("=" * 70)
+        print(f"🔍 Type de corrélation utilisée : Chi2")
+        print(f"📊 V de Cramer : {v_cramer:.4f}, corrélation {'faible' if v_cramer <= 0.1 else 'modérée' if 0.1 < v_cramer <= 0.3 else 'forte'}")
+        print(f"📊 P-value : {p_value:.4f}")
+
         if p_value < seuil:
             print(f"\n✅ CONCLUSION: Il existe une corrélation statistiquement significative entre '{var1}' et '{var2}' (p < {seuil}).")
         else:
